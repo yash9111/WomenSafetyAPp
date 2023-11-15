b37c61c2613df5860f9efd77e97ae9521910d5e0
Yash pathak <assush698@gmail.com>
2023-11-15 18:56:05 +0530

    Improved security

--- Frontend/lib/DBhelper/consts.dart
+++ Frontend/lib/DBhelper/consts.dart
@@ -1,7 +1,9 @@
-const USER_COLLECTION = "BlogPosts";
+
+
+const USER_COLLECTION = "YOUR_MONGO_DB_COLLECTION_NAME";
 const DB_CONNECTION_URL =
-    "mongodb+srv://ypathak188:DZHSMs5KUHroJKIS@kanafusi.80styik.mongodb.net/Women_Safety?retryWrites=true&w=majority";
+    "YOUR_MONGODB_URL";
 
 //GITHUB PERSONAL TOKEN
 
-const GITHUB_TOKEN = "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";
+const GITHUB_TOKEN = "GITHUB_ACCESS_TOKEN";
